4b3a3e01c10f6c79f9ab4fc1c395663ed9181987
luweihai <644475880@qq.com>
2019-01-19 20:44:21 +0800

    最小栈、螺旋遍历矩阵                       终于到家啦 娇娇的父母也很喜欢我  我也喜欢娇娇  是不是该准备结婚了啊 哈哈哈  2019年1月19日

---
+++ Solution/【054】螺旋矩阵/螺旋矩阵.java
@@ -0,0 +1,44 @@
+/*
+（1）设置 up down left right 四个指针，分别表示在上 下 左 右 四个部分移动的情况。每个指针移动一次，就要 -1
+（2）注意好边界条件，比如 先 上 后 右 ，再执行 下 的时候，可能会导致一行遍历2次，所以要进行判断。
+*/
+
+
+
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> result = new ArrayList<>();
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0)
+            return result;
+        int up = 0;
+        int down = matrix.length - 1;
+        int left = 0;
+        int right = matrix[0].length -1 ;
+        while(left <= right && up <= down){
+            for(int i = left ; i <= right ; i++){
+                result.add(matrix[up][i]);
+            }
+            up ++;
+            
+            for(int i = up ; i <= down ; i++){
+                result.add(matrix[i][right]);
+            }
+            right --;
+            
+            if( up <= down ){            // 此处是关键点， 下  判断，避免重复
+                for(int i = right ; i >= left ; i--){
+                    result.add(matrix[down][i]);
+                }
+            }
+            down --;
+            
+            if( left <= right){          // 此处是关键点， 左 判断，避免重复
+                for(int i = down ; i >= up ; i--){
+                    result.add(matrix[i][left]);
+                }
+            }
+            left ++;
+        }
+        return result;
+    }
+}
---
+++ Solution/【155】最小栈/最小栈.java
@@ -0,0 +1,50 @@
+/*
+首先，初始化的构造方法是可以为空的，只要在前面初始化了，当然也可以留在构造方法中初始化
+此外，要理解题目的思路。
+我们要用两个栈实现，一个 stack 和 一个 minstack 
+最直观的思路是，每次判断入栈的元素和当前最小栈 minstack 的栈顶元素的大小，如果小于minstack 栈顶元素就把与minstack 的栈顶元素再次入 minstack
+这样可能 minstack 中多个元素相同，而且保持了与 stack中元素的同步
+要取出最小的元素，就直接 minstack.peek()即可
+*/
+
+
+class MinStack {
+    Stack<Integer> stack = new Stack<>();
+    Stack<Integer> minstack =  new Stack<>();
+    /** initialize your data structure here. */
+    public MinStack() {
+        
+    }
+    
+    public void push(int x) {
+        if(minstack.isEmpty() == true || minstack.peek() > x ){
+            minstack.push(x);
+        }
+        else{
+            minstack.push(minstack.peek());
+        }
+        stack.push(x);
+    }
+    
+    public void pop() {
+        minstack.pop();
+        stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek();
+    }
+    
+    public int getMin() {
+        return minstack.peek();
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
