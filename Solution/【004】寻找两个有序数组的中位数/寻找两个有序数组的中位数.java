388c3320709c28600791b2397fd0d23d92472c13
luweihai <644475880@qq.com>
2019-10-26 22:53:45 +0800

    中位数

--- Solution/【004】寻找两个有序数组的中位数/寻找两个有序数组的中位数.java
+++ Solution/【004】寻找两个有序数组的中位数/寻找两个有序数组的中位数.java
@@ -1,46 +1,43 @@
 /*
-（1）实现一个寻找两个数组中第 k 小的算法，此处是关键
-    要注意这个判断数组 A 和 B 的第 k/2 位置的比较，决定了后续要在哪个数组中寻找结果 
-（2）寻找第 mid 点偏左边的点，再寻找第 mid 点偏右边的点，最后将两个点加起来 * 1.0 然后除以2 即可 
+https://leetcode-cn.com/problems/median-of-two-sorted-arrays/solution/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by-w-2/    这个解答很好，仔细看看图并且理解
+核心思想是 两个排序数组 求其 topk 的方法。这个方法说不定以后也会用，一定要掌握
+
 */
 
-
 class Solution {
-    public static double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int length1 = nums1.length;
-        int length2 = nums2.length;
-        int lengthall = length1 + length2;
-        int l = (lengthall + 1) / 2;    // 中间点偏左边的位置
-        int r = (lengthall + 2) / 2;    // 中间点偏右边的位置
-        int mid_Left = getKMin(nums1 , 0 , nums2 , 0 , l);    // 得到中位数偏左的数
-        int mid_Right = getKMin(nums1 , 0 , nums2, 0 , r);    // 得到中位数偏右的数
-        double result = (mid_Left + mid_Right) * 1.0 / 2;     // 这里一定要 * 1.0 不然会整除了
-        return result;
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+        int left = (n + m + 1) / 2;
+        int right = (n + m + 2) / 2;
+        //将偶数和奇数的情况合并，如果是奇数，会求两次同样的 k 。
+        return (getKth(nums1, 0, n - 1, nums2, 0, m - 1, left) + getKth(nums1, 0, n - 1, nums2, 0, m - 1, right)) * 0.5;  
     }
-    public static int getKMin(int[] A, int Astart, int[] B, int Bstart, int k){
-        if (Astart > A.length - 1){    // 如果在 A中开始的索引超过了A 的长度
-            return B[Bstart + k -1];   // 就在 B中找第 K 小的元素
+    
+    private int getKth(int[] nums1, int start1, int end1, int[] nums2, int start2, int end2, int k) {
+        int len1 = end1 - start1 + 1;
+        int len2 = end2 - start2 + 1;
+        //让 len1 的长度小于 len2，这样就能保证如果有数组空了，一定是 len1 
+        if (len1 > len2) 
+            return getKth(nums2, start2, end2, nums1, start1, end1, k);
+        if (len1 == 0) 
+            return nums2[start2 + k - 1];
+
+        if (k == 1) 
+            return Math.min(nums1[start1], nums2[start2]);
+
+        int i = start1 + Math.min(len1, k / 2) - 1;
+        int j = start2 + Math.min(len2, k / 2) - 1;
+
+        if (nums1[i] > nums2[j]) {
+            return getKth(nums1, start1, end1, nums2, j + 1, end2, k - (j - start2 + 1));
         }
-        if (Bstart>B.length-1){       // 如果在 B 中开始的索引超过了B 的长度
-            return A[Astart + k -1];   //  就在 A 中找第 K 小的元素
+        else {
+            return getKth(nums1, i + 1, end1, nums2, start2, end2, k - (i - start1 + 1));
         }
-        if (k == 1){                // k == 1的时候比较 A B开始索引的元素
-            return Math.min(A[Astart],B[Bstart]);
-        }
- 
-        int Amin = Integer.MAX_VALUE , Bmin = Integer.MAX_VALUE;
-        if (Astart + k/2 - 1 < A.length){   // 往右边取 k/2 位置的元素
-            Amin = A[Astart + k/2 -1];     //  A 数组在上述位置的值
-        }
-        if (Bstart + k/2 - 1 < B.length){  // B 数组在上述位置的值
-            Bmin = B[Bstart + k/2 -1];
-        }
-        if(Amin < Bmin)      // 如果在上述位置，A 的值小于B 的值，则说明要找的第 k 小的点在B 这边
-            return getKMin(A , Astart + k/2 , B , Bstart , k - k/2);
-        else                 // 否则说明在 A 这边
-            return getKMin(A , Astart , B , Bstart + k/2 , k - k/2);
     }
 }
+
 
 
 
@@ -60,3 +57,73 @@ class Solution {
         }
     }
 }
+
+
+
+// 申请一个新数组，然后将两个有序数组进行合并
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int length_1 = nums1.length;
+        int length_2 = nums2.length;
+        int[] nums = new int[length_1 + length_2];
+        if(length_1 == 0){
+            int mid = length_2 / 2;
+            if(length_2 % 2 == 0){
+                return (nums2[mid] + nums2[mid - 1]) / 2.0;
+            }
+            else{
+                return nums2[mid];
+            }
+        }
+        if(length_2 == 0){
+            int mid = length_1 / 2 ;
+            if(length_1 % 2 == 0){
+                return (nums1[mid] + nums1[mid - 1]) / 2.0;
+            }
+            else{
+                return nums1[mid];
+            }
+        }
+        int count = 0;
+        int i = 0, j = 0;
+        while(count < (length_1 + length_2)){
+            if(i == length_1){
+                while(j != length_2){
+                    nums[count] = nums2[j];
+                    count ++;
+                    j ++;
+                }
+                break;
+            }
+            if(j == length_2){
+                while(i != length_1){
+                    nums[count] = nums1[i];
+                    count ++;
+                    i ++;
+                }
+                break;
+            }
+            if(nums1[i] < nums2[j]){
+                nums[count] = nums1[i];
+                count ++;
+                i ++;
+            }
+            else{
+                nums[count] = nums2[j];
+                count ++;
+                j ++;
+            }
+        }
+        if (count % 2 == 0) {
+            return (nums[count / 2 - 1] + nums[count / 2]) / 2.0;
+        } 
+        else {
+            return nums[count / 2];
+        }
+    }
+}
+
+
+
+
+
