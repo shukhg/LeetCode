f1b2223527c785ebde9acd95cd901ea2973c26b0
luweihai <644475880@qq.com>
2019-10-30 00:21:43 +0800

    Z变换 Java

--- Solution/【006】Z 字形变换/Z 字形变换.java
+++ Solution/【006】Z 字形变换/Z 字形变换.java
@@ -1,5 +1,10 @@
 /*
 *  想清楚里面一步步的逻辑
+（1）计算出 第一行 和 最后一行中 两列 之间的元素间隔
+（2）填充第一行和最后一行
+（3）计算出 中间行 的两列之间的元素间隔  k2 = k1 - 2 * i
+（4）注意 上述中间行间隔是在两个值之间变化的
+（5）注意 numRows == 1 的特殊情况
 */
 
 
@@ -7,27 +12,28 @@
 
 class Solution {
     public String convert(String s, int numRows) {
-        if(s == null || s.length() <= numRows || numRows == 1)
-            return "";
+        if(s == null || s.length() <= numRows || numRows == 1){
+            return s;
+        }
         String result = "";
-        int k1 = 2 * numRows - 2;   // k1 表示 第一行 和 最后一行 相邻两个元素之间差的元素
-		int index = 0;
-		for(int i = 0 ; i < numRows ; i ++){
-            if(i == 0 || i == numRows - 1){
-                result = result + s.charAt(i);
-                index = i + k1;                // 取第一行或者最后一行下一个元素
+        int k1 = 2 * numRows - 2; // k1 表示第一行和最后一行 相邻的两个元素之间差的元素
+        int index = 0;
+        for(int i = 0; i < numRows; i ++){    // 遍历每一行
+            if(i == 0 || i == numRows - 1){    // 第一行或者最后一行
+                result = result + s.charAt(i);   // 注意这里都是从 s 中的 i 开始
+                index = i + k1;     // 取第一行或者最后一行 在 s 中的下一个元素
                 while(index < s.length()){
                     result = result + s.charAt(index);
                     index = index + k1;
                 }
             }
             else{
-                result = result + s.charAt(i);          // 注意这里是 i ，一开始的时候是从 i 位置开始
-                int k2 = k1 - 2 * i;
+                result = result + s.charAt(i);
+                int k2 = k1 - 2 * i;      // 中间行的距离 和 k1 以及 第几行都是有关的
                 index = i + k2;
                 while(index < s.length()){
                     result = result + s.charAt(index);
-                    k2 = k1 - k2;                   // 这里要注意，每循环一次 k2 的值会变
+                    k2 = k1 - k2;   // 注意这里，每层的 k2 是在两个值之间切换的
                     index = index + k2;
                 }
             }
