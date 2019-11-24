7817d2accdfdb87aefff0ff80adae9f792138a53
luweihai <644475880@qq.com>
2019-11-24 23:59:09 +0800

    new start

--- Solution/【001】两数之和/两数之和.cpp
+++ Solution/【001】两数之和/两数之和.cpp
@@ -1,20 +1,18 @@
 
 
-
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-
         vector<int> result;
-        map<int, int> hashmap;
-        for(int i = 0; i < nums.size(); i ++){
+        map<int, int> my_map;
+        for(int i = 0; i <= nums.size(); i++){
             int div = target - nums[i];
-            if(hashmap.find(div) != hashmap.end()){
-                result.push_back(hashmap[div]);
+            if(my_map.find(div) != my_map.end()){
+                result.push_back(my_map[div]);
                 result.push_back(i);
                 return result;
             }
-            hashmap[nums[i]] = i;
+            my_map[nums[i]] = i;
         }
         return result;
     }
