2558c7fc8de52c44e12dc1b233b6d67f5b7e86f8
luweihai <644475880@qq.com>
2019-12-04 13:08:17 +0800

    2019-12-04 13

---
+++ Solution/【035】搜索插入位置/搜索插入位置.cpp
@@ -0,0 +1,26 @@
+
+
+
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        if(nums.empty() || nums.size() == 0){
+            return 0;
+        }
+        int left = 0, right = nums.size() - 1;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            else if(nums[mid] > target){
+                right = mid - 1;
+            }
+            else{
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+};
