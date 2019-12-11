5c9a4ace8becfb159f78dfb680a6d7a16d57b9fb
luweihai <644475880@qq.com>
2019-12-11 13:58:54 +0800

    2019-12-11 14

--- Solution/【062】不同路径/不同路径.java
+++ Solution/【062】不同路径/不同路径.java
@@ -1,6 +1,30 @@
 /*
 简单的动态规划，别看错题目就可以了
+
+但是可以有更好的解法。
+因为遍历的时候是一行一行遍历，其实只要用一个一维数组去存储就可以了。
+dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+dp[i - 1][j] 其实就是上一行的 dp 数组中的数据
+可以改为  dp[j] = dp[j] + dp[j - 1]   这里的  dp[j] 实现了上一行
 */
+
+
+// 降低空间复杂度
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[] dp = new int[m];
+        Arrays.fill(dp, 1);
+        for(int i = 1; i < n; i ++){
+            for(int j = 1; j < m; j ++){
+                dp[j] = dp[j] + dp[j - 1];
+            }
+        }
+        return dp[m - 1];
+    }
+}
+
+
+
 
 class Solution {
     public int uniquePaths(int cols, int rows) {
--- Solution/【063】不同路径Ⅱ/不同路径Ⅱ.java
+++ Solution/【063】不同路径Ⅱ/不同路径Ⅱ.java
@@ -5,38 +5,37 @@
 
 
 class Solution {
-    public int uniquePathsWithObstacles(int[][] matrix) {
-        int rows = matrix.length , cols = matrix[0].length;
-        int[][] dp = new int[rows][cols];
-        if(matrix[0][0] == 1)
-            return 0;
-        dp[0][0] = 1;
-        for(int i = 1 ; i < rows ; i ++){
-            if(matrix[i][0] == 0){
-                dp[i][0] = dp[i - 1][0];
-            }
-            else{
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int n = obstacleGrid.length, m = obstacleGrid[0].length;
+        int[][] dp = new int[n][m];
+        if(obstacleGrid[0][0] == 1) return 0;
+        else    dp[0][0] = 1;
+        for(int i = 1; i < n; i ++){
+            if(obstacleGrid[i][0] == 1){
                 dp[i][0] = 0;
             }
+            else{
+                dp[i][0] = dp[i - 1][0];
+            }
         }
-        for(int i = 1 ; i < cols ; i ++){
-            if(matrix[0][i] == 0){
-                dp[0][i] = dp[0][i - 1];
+        for(int j = 1; j < m; j ++){
+            if(obstacleGrid[0][j] == 1){
+                dp[0][j] = 0;
             }
             else{
-                dp[0][i] = 0;
+                dp[0][j] = dp[0][j - 1];
             }
         }
-        for(int i = 1 ; i < rows ; i ++){
-            for(int j = 1 ; j < cols ; j ++){
-                if(matrix[i][j] == 0){
-                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
-                }
-                else{
+        for(int i = 1; i < n; i ++){
+            for(int j = 1; j < m; j ++){
+                if(obstacleGrid[i][j] == 1){
                     dp[i][j] = 0;
                 }
+                else{
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
             }
         }
-        return dp[rows - 1][cols - 1];
+        return dp[n - 1][m - 1];
     }
 }
