8799563c1adde0dd24860aa5e92fa2f64d32c147
luweihai <644475880@qq.com>
2019-12-13 13:59:18 +0800

    2019-12-13 14

--- Solution/【072】编辑距离/编辑距离.java
+++ Solution/【072】编辑距离/编辑距离.java
@@ -4,33 +4,28 @@
 */
 
 
-
 class Solution {
     public int minDistance(String word1, String word2) {
-        int length_1 = word1.length();
-        int length_2 = word2.length();
-        if(length_1 == 0){
-            return length_2;
-        }
-        if(length_2 == 0)
-            return length_1;
-        int[][] dp = new int[length_1 + 1][length_2 + 1];  // 表示word1从0 到 i子串 与 word2从0 到 j子串 的编辑距离
-        for(int i = 0 ; i <= length_1 ; i ++){
+        int len1 = word1.length(), len2 = word2.length();
+        if(len1 == 0)   return len2;
+        if(len2 == 0)   return len1;
+        int[][] dp = new int[len1 + 1][len2 + 1];  // 0 到 i 所以是 len + 1
+        for(int i = 0; i <= len1; i ++){
             dp[i][0] = i;
         }
-        for(int i = 0 ; i <= length_2 ; i ++){
-            dp[0][i] = i;
+        for(int j = 0; j <= len2; j ++){
+            dp[0][j] = j;
         }
-        for(int i = 1 ; i <= length_1 ; i ++){
-            for(int j = 1 ; j <= length_2 ; j ++){
-                if(word1.charAt(i - 1) == word2.charAt(j - 1)){  // 注意这里是 i - 1，要从第一个字符开始
+        for(int i = 1; i <= len1; i ++){
+            for(int j = 1; j <= len2; j ++){
+                if(word1.charAt(i - 1) == word2.charAt(j - 1)){
                     dp[i][j] = dp[i - 1][j - 1];
                 }
                 else{
-                    dp[i][j] = Math.min(dp[i - 1][j - 1] , Math.min(dp[i - 1][j] , dp[i][j - 1]) ) + 1;
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
                 }
             }
         }
-        return dp[length_1][length_2];
+        return dp[len1][len2];
     }
 }
--- Solution/【073】矩阵置零/矩阵置零.java
+++ Solution/【073】矩阵置零/矩阵置零.java
@@ -1,60 +1,41 @@
 /*
-（1）利用第一行和第一列进行存储，不用额外的空间
-（2）首先，记录第一行和第一列是否有 0 
-（3）然后，在矩阵中（除了第一行和第一列）查找0，如果有 0 就把 0 在第一行和第一列的投影的位置设置为 0
-（4）之后，查找第一行和第一列，发现 0 的话，就把相应的行和列全部设置为0
-（5）最后，根据第二步的记录，决定是否把第一行和第一列全部设置为 0
+利用 matrix[0][0] 作为第一行的标记，再设置一个 col_flag 作为第一列的标记
 */
 
 
-public class Solution {
+class Solution {
     public void setZeroes(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        Boolean first_row = false;    // 判断第一行是否有为0的元素
-        Boolean first_col = false;    // 判断第一列是否有为0的元素
-        for (int i = 0; i < m; i++){    // 第一行判断
-            if (matrix[i][0] == 0){
-                first_col = true;
-                break;
+        int rows = matrix.length, cols = matrix[0].length;
+        boolean col_flag = false;
+
+        for(int i = 0; i < rows; i ++){
+            if(matrix[i][0] == 0){   // 第一列的记录，这里考虑了 m[0][0] 位置
+                col_flag = true;
             }
-        }
-        for (int j = 0; j < n; j++){   // 第一列判断
-            if (matrix[0][j] == 0){
-                first_row = true;
-                break;
-            }
-            
-        }
-        for (int i = 1; i < m; i++){        //在矩阵中（非第一行第一列）查找 0
-            for (int j = 1; j < n; j++){     
-                if (matrix[i][j] == 0){     // 把这个点在第一行和第一列的投影设置为0
-                    matrix[0][j] = 0;
+            for(int j = 1; j < cols; j ++){  // 其他列的记录，这里的 i 是可以取到 0 的，所以可以用 m[0][0] 位置来表示第一行
+                if(matrix[i][j] == 0){
                     matrix[i][0] = 0;
+                    matrix[0][j] = 0;
                 }
             }
         }
-        for (int i = 1; i < m ; i++){        // 查找第一列是否有 0
-            if (matrix[i][0] == 0){          // 如果有的话，就把 0 对应的行设置为 0
-                for (int j = 1; j < n; j++){
+
+        for(int i = 1; i < rows; i ++){
+            for(int j = 1; j < cols; j ++){
+                if(matrix[i][0] == 0 || matrix[0][j] == 0){
                     matrix[i][j] = 0;
                 }
             }
         }
-        for (int j = 1; j < n ; j++){       // 查找第一行是否有 0 
-            if (matrix[0][j] == 0){          // 如果有的话，就把 0 对应的列设置为 0
-                for (int i = 1; i < m; i++){
-                    matrix[i][j] = 0;
-                }
-            }
-        }
-        if (first_row){                        //  如果第一行有 0 就把第一行全设置为 0
-            for (int j = 0; j < n; j++){
+
+        if(matrix[0][0] == 0){
+            for(int j = 0; j < cols; j ++){
                 matrix[0][j] = 0;
             }
         }
-        if (first_col){                    // 如果第一列有 0 就把第一列全设置为 0
-            for (int i = 0; i < m; i++){
+
+        if(col_flag){
+            for(int i = 0; i < rows; i ++){
                 matrix[i][0] = 0;
             }
         }
