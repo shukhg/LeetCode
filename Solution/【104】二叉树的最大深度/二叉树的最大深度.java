4e41f8657e3342815961438ad86952c5c79669ca
luweihai <644475880@qq.com>
2019-12-19 14:15:27 +0800

    2049-12-19 1415

--- Solution/【104】二叉树的最大深度/二叉树的最大深度.java
+++ Solution/【104】二叉树的最大深度/二叉树的最大深度.java
@@ -4,17 +4,10 @@
 
 class Solution {
     public int maxDepth(TreeNode root) {
-        if(root == null) 
-            return 0;
-        if(root.left == null && root.right == null) 
-            return 1;
-        int left = maxDepth(root.left);
-        int right = maxDepth(root.right);
-        if(left > right){
-            return left + 1;
-        }
-        else{
-            return right + 1;
-        }
+        if(root == null)    return 0;
+        if(root.left == null && root.right == null) return 1;
+        int left_depth = maxDepth(root.left);
+        int right_depth = maxDepth(root.right);
+        return Math.max(left_depth, right_depth) + 1;
     }
 }
