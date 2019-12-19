9526f3ae068703ff8f52a117f9fbea45ceb5eba4
luweihai <644475880@qq.com>
2019-12-19 12:21:34 +0800

    2019-12-19 12

--- Solution/【098】验证二叉搜索树/验证二叉搜索树.java
+++ Solution/【098】验证二叉搜索树/验证二叉搜索树.java
@@ -1,7 +1,28 @@
 /*
-* 显然是中序遍历，但是要注意，要判断遍历的时候前一个结点的值比后一个结点的值小
+显然是中序遍历，但是要注意，要判断遍历的时候前一个结点的值比后一个结点的值小
+但是实现方式可以不同，其实没必要利用一个  list 去存储中序遍历的结果，只需要模拟中序遍历的过程然后利用一个 pre 指针保存中序遍历的上一个结点即可
 */
 
+
+class Solution {
+    TreeNode pre = null;    // 用 pre 记录中序遍历的上一个结点
+    public boolean isValidBST(TreeNode root) {
+        if(root == null)    return true;
+        boolean judge_left = isValidBST(root.left);
+        if(judge_left == false) return false;
+        if(pre != null && pre.val >= root.val)  return false;
+        pre = root;
+        boolean judge_right =isValidBST(root.right);
+        return judge_right;
+    }
+}
+
+
+
+
+
+
+// 利用 list 存储
 class Solution {
     List<Integer> list = new ArrayList<>();   // 注意此处是全局的 list
     public boolean isValidBST(TreeNode root) {
