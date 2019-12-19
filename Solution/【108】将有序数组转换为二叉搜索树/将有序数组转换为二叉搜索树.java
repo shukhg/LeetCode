66c07ef530763c7ec563d3d967cb3707938b0c31
luweihai <644475880@qq.com>
2019-12-19 14:43:48 +0800

    2049-12-19 14 40

--- Solution/【108】将有序数组转换为二叉搜索树/将有序数组转换为二叉搜索树.java
+++ Solution/【108】将有序数组转换为二叉搜索树/将有序数组转换为二叉搜索树.java
@@ -1,26 +1,19 @@
 /*
 每次找到 left 与 right 的中间点，递归地创建。
-类似于深度优先搜索的代码风格
 */
 
 class Solution {
     public TreeNode sortedArrayToBST(int[] nums) {
-        if(nums == null || nums.length == 0)  
-            return null;
-        TreeNode root = create(nums , 0 , nums.length - 1);
+        if(nums == null || nums.length == 0)    return null;
+        TreeNode root = build(nums, 0, nums.length - 1);
         return root;
     }
-    public TreeNode create(int[] nums , int left , int right){
-        if(nums == null || nums.length == 0 ) 
-            return null;
-        int mid = (left + right) / 2;
-        if(left > right)
-            return null;
-        
-        mid = (left + right) / 2;
+    public TreeNode build(int[] nums, int left, int right){
+        if(left > right)    return null;
+        int mid = left + (right - left) / 2;
         TreeNode root = new TreeNode(nums[mid]);
-        root.left = create(nums , left , mid - 1);
-        root.right = create(nums , mid + 1 , right);
+        root.left = build(nums, left, mid - 1);
+        root.right = build(nums, mid + 1, right);
         return root;
     }
 }
