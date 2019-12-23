a68fa73acba09ccaf3bacb6211459d06e79a649c
luweihai <644475880@qq.com>
2019-12-23 14:57:02 +0800

    2019-12-23 13

--- Solution/【124】二叉树中的最大路径和/二叉树中的最大路径和.java
+++ Solution/【124】二叉树中的最大路径和/二叉树中的最大路径和.java
@@ -1,43 +1,23 @@
 /*
-递归地遍历左子树和右子树，注意往下层走的时候，只能往左子树+根 或者 右子树+根 的组合，而不能左子树加右子树加根。
-但是判断当前最大值的时候，可以是左子树+根+右子树
-遇到小于0 的，不要添加到当前值的组合中
-比如左子树求的为负数，就 root.val + rightMax
+（1）全局变量 result 记录结果，遇到每个结点的时候都判断 result 是否需要更新
+（2）搜索的函数的功能是 找到 root 为根结点的左边 或者 右边 的一条最大路径，只能经过左边+根 或者 右边+根。因为可能有负值，所以需要判断与 0 的大小
 */
 
 
 
 class Solution {
-    int max = Integer.MIN_VALUE;
+    int result = Integer.MIN_VALUE;
     public int maxPathSum(TreeNode root) {
-        if(root == null )
-            return 0;
-        maxSum(root );
-        return max;
+        search(root);
+        return result;
     }
-    public int maxSum(TreeNode root  ){
-        int leftMax = -111111;   // 注意此处 要是从int最小值开始，传到下层会有错误
-        int rightMax = -2221111;
-        int value = root.val;
-        if(root.left != null){
-            leftMax = maxSum(root.left  );
-            if(leftMax > 0 ){
-                value = value + leftMax;
-            }
-        }
-        if(root.right != null){
-            rightMax = maxSum(root.right);
-            if( rightMax > 0)
-                value  = value + rightMax;
-        }
-        //max is the max of {root.val,root.val+lmax,root.val+rmax, root.val + lmax + rmax}
-        if(value > max)
-            max = value;
-        
-        //return max of (root.val, root.val + lmax, root.val + rmax)
-        return Math.max(root.val,Math.max(root.val + leftMax , root.val + rightMax));
-        //注意此处往下层走的时候只能往一边走，不能同时向左和向右
+    public int search(TreeNode root){   // 寻找 root分别与左右两子树走的最长路径
+        if(root == null)    return 0;
+        int left = search(root.left);
+        int right = search(root.right);
+        result = Math.max(left + right + root.val, result);  // 遇到每个结点都更新 result
+        int left_right_max = Math.max(left, right);
+        return Math.max(0, left_right_max + root.val);
     }
 }
-
 
