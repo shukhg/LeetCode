075770c5a72da26a4e2e035d6b3c1b0a71074d97
luweihai <644475880@qq.com>
2019-12-25 14:20:18 +0800

    2019-12-25 14“

--- Solution/【131】分割回文串/分割回文串.java
+++ Solution/【131】分割回文串/分割回文串.java
@@ -1,39 +1,39 @@
 /*
-此题的思路很好理解，就和普通的回溯差不多，但是有以下几点要注意
-(1)判断是否是回文字符串的时候，尽量用一半字符的方法，而不是逆置再equals()
-(2)dfs中循环遍历是没有 start的，此处采用不断更新的 s ，每次循环的终点是 s.length()
-    循环变量对应了一个个子串。  记住这种操作，在用到字符串子串的时候经常使用
+（1）先写个函数判断一个字符串是否为回文串
+（2）剩下的都是常规的 dfs 的框架
 */
 
 
 
-
+// 每次遍历到一个字符都判断
 class Solution {
     public List<List<String>> partition(String s) {
         List<List<String>> result = new ArrayList<>();
-        if(s == null || s.length() == 0)
-            return result;
-        dfs(result , s , new ArrayList<String>() );
+        if(s == null || s.length() == 0)    return result;
+        dfs(result, s, new ArrayList<String>(), 0);
         return result;
     }
-    public void dfs(List<List<String>> result , String s , List<String> list ){
-        if( s.length() == 0 ) {
-            result.add(new ArrayList<String>(list));
+    public void dfs(List<List<String>> result, String s, List<String> temp, int index){
+        if(index == s.length()){
+            result.add(new ArrayList<String>(temp));
             return ;
-        }    // s 在此处也是不断更新，每次都是一个子串
-        for(int i = 0 ; i < s.length() ; i ++){  // 此处没用 start，而是用了 substring()
-            if( judge(s.substring(0 , i + 1) ) ){
-                list.add(s.substring(0, i + 1 ));
-                dfs(result , s.substring( i + 1 ) , list );   // 注意此处更新后的 s 是剩余部分的 s
-                list.remove(list.size() - 1);
+        }
+        for(int i = index; i < s.length(); i ++){
+            if(judge(s.substring(index, i + 1))){
+                temp.add(s.substring(index, i + 1));
+                dfs(result, s, temp, i + 1);
+                temp.remove(temp.size() - 1);
             }
         }
     }
-    public boolean judge(String s){  // 最好是用这个判断是否是回文串，如果全部生成逆置字符串再判断会超出内存
-        for(int i = 0 ; i < s.length() / 2 ; i ++){
-            if(s.charAt(i) != s.charAt(s.length() - 1 - i)){
+    public boolean judge(String s){
+        int left = 0, right = s.length() - 1;
+        while(left <= right){
+            if(s.charAt(left) != s.charAt(right)){
                 return false;
             }
+            left ++;
+            right --;
         }
         return true;
     }
--- Solution/【132】分割回文串 II/分割回文串 II.java
+++ Solution/【132】分割回文串 II/分割回文串 II.java
@@ -1,79 +1,33 @@
 /*
-后面还有个方法2，有时候会超出内存限制。
+（1）基于之前的找到最长回文子串的方法，利用 二维 dp 存储一个字串是否是回文串
+（2）利用 min_s 存储从 0 位置 到 i 位置的子串的最小分割次数，初始化都是 i，因为可以分割成一个个单字符
+（3）j 是子串起始位置，如果 j == 0 则表明此子串整个都为回文串，所有 min_s[i] = 0，否则 为 原先分割方法次数 与 0 到 j - 1 的分割方法次数 + 1（从 j 到 i 所以需要 +1 ）
 */
-
-
- /*
- 
- dp二维数组dp[i][j]表示字符串[i,j]从第i个位置（包含）到第j个位置（包含） 是否是回文。
-
- 如何判断字符串[i,j]是不是回文？
-
- 1. dp[i+1][j-1]是回文且 s.charAt(i) == s.charAt(j)。
-
- 2. i==j（i，j是用一个字符）
-
- 3. j=i+1（i，j相邻）且s.charAt(i) == s.charAt(j)
-
- 当字符串[i,j]是回文后，说明从第i个位置到字符串第len位置的最小count数可以被更新了，
-
- 即，Math.min(count[i], count[j+1] + 1) 
-
- 最后返回count[0] - 1。把多余加的那个对于第len位置的切割去掉，即为最终结果。
- */
-
-
- 
-public class Solution {
-    public int minCut(String s) {
-        int[][] dp=new int[s.length()][s.length()];   // 表示两个元素之间形成的子串是否是回文串 
-        int[] count=new int[s.length() + 1];    // 表示从位置i开始到最后的最小划分个数，包含了 len下标的位置
-        
-        for(int i = s.length() - 1 ; i >= 0 ; i --)  // 从后往前走
-        {
-            count[i] = Integer.MAX_VALUE;
-            for(int j = i ; j < s.length() ; j ++)    // 因为 dp[i + 1][j - 1] == 1 限制了 i j 遍历的顺序 
-            {
-                if(s.charAt(i) == s.charAt(j) && (j - i < 2 || dp[i + 1][j - 1] == 1))  // 注意此处是否是回文串的判断 
-                {
-                	dp[i][j] = 1;
-                	count[i] = Math.min(1 + count[j + 1] , count[i]);  // 状态转移方程
-                }
-            }
-        }
-        return count[0] - 1;       // 因为是倒着走的，把多余加的那个对于第len位置的切割去掉，即为最终结果
-    }
-}
-
 
 
 
 
 class Solution {
     public int minCut(String s) {
-        if(s == null || s.length() == 0)
-            return 0;
-        int[] dp = new int[s.length()]; //dp[i]存放(0,i)即以i的字符结束的子串的最小切割数
-        dp[0] = 0;                      // 一个字符不需要切割
-        for(int i = 1 ; i < s.length() ; i ++){
-            if( judge(s.substring(0 , i + 1 )) == true )  //下标 0 到 i 的子串为回文串
-                dp[i] = 0;                               // 下标 0 到 i 不用切割 
-            else
-                dp[i] = i  ;                             // 最多需要切割 i  次
-            for(int j = i ; j >= 1 ; j --){                // 再从 i 开始从后往前遍历，j = 0就没意义了
-                if(judge( s.substring( j , i + 1 )) == true )   // 下标 j 到 i 为回文串
-                    dp[i] = Math.min(dp[i] , dp[j - 1 ] + 1 );   // 更新 dp[i]
+        int len = s.length();
+        int[] min_s = new int[len];
+        boolean[][] dp = new boolean[len][len];
+        for(int i = 0; i < len; i ++){
+            min_s[i] = i;
+            for(int j = 0; j <= i; j ++){
+                if(s.charAt(i) == s.charAt(j) && (i - j < 2 || dp[j + 1][i - 1])){
+                    dp[j][i] = true;
+                    if(j == 0){
+                        min_s[i] = 0;
+                    }
+                    else{
+                        min_s[i] = Math.min(min_s[i], min_s[j - 1] + 1);
+                    }
+                }
             }
         }
-        return dp[s.length() - 1];
-    }
-    public boolean judge(String s ){
-        if(s == null || s.length() <= 1)
-            return true;
-        for(int i = 0 ; i < s.length() / 2 ; i ++){
-            if(s.charAt(i) != s.charAt(s.length() - 1 - i))
-                return false;
-        }
-        return true;
+        return min_s[len - 1];
     }
 }
+
+
