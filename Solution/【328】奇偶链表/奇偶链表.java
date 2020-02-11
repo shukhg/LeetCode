5d2a21897f79ee9ad07cccb9c2cdff651acef2ca
luweihai <644475880@qq.com>
2020-02-11 14:03:36 +0800

    2020-02-11 14

--- Solution/【328】奇偶链表/奇偶链表.java
+++ Solution/【328】奇偶链表/奇偶链表.java
@@ -1,5 +1,9 @@
 /*
-将奇节点放在一个链表里，偶链表放在另一个链表里。然后把偶链表接在奇链表的尾部
+（1）将奇节点放在一个链表里，偶链表放在另一个链表里。然后把偶链表接在奇链表的尾部
+（2）直接用 o 和 e 在循环中去迭代，最后 return head 就可以了
+（3）while 循环中是 o.next != null && e.next != null
+（4）直接 o.next = e.next; o = o.next; e.next = o.next; e = e.next; 
+（5）最后 o.next = p，此处的 p 是一开始初始化的 head.next，不随着循环进行遍历
 */
 
 
