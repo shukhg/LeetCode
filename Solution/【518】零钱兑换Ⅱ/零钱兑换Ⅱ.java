8c4e4fc126b1a3b0ef0404796db229b6117b5563
luweihai <644475880@qq.com>
2020-02-12 16:09:52 +0800

    2020-02-12 16

--- Solution/【518】零钱兑换Ⅱ/零钱兑换Ⅱ.java
+++ Solution/【518】零钱兑换Ⅱ/零钱兑换Ⅱ.java
@@ -8,37 +8,38 @@
                 dp[i - 1][j - k * coins[i]]
 （3）更高效版状态转移方程  dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i]]
     这个状态转移方程可以理解为 递归
+（4）初始化：amout == 0 则全 1，coins 选第一个的时候则将其整数倍 置 1
 */
 
 
-public class Solution {
 
+
+class Solution {
     public int change(int amount, int[] coins) {
-        int len = coins.length;
-        if (len == 0) {
-            if (amount == 0) {
-                return 1;
-            }
-            return 0;
+        if(coins == null || coins.length == 0){
+            if(amount == 0) return 1;
+            else    return 0;    
         }
-        int[][] dp = new int[len][amount + 1];
-        dp[0][0] = 1;
-
-        for (int i = coins[0]; i <= amount; i += coins[0]) {
-            dp[0][i] = 1;
+        int[][] dp = new int[coins.length][amount + 1];
+        for(int i = 0; i < coins.length; i ++){
+            dp[i][0] = 1;
         }
-
-        for (int i = 1; i < len; i++) {
-            for (int j = 0; j <= amount; j++) {
+        for(int j = coins[0]; j <= amount; j = j + coins[0]){
+            dp[0][j] = 1;
+        }
+        for(int i = 1; i < coins.length; i ++){
+            for(int j = 1; j <= amount; j ++){
                 // for (int k = 0; j - k * coins[i] >= 0; k++) {     此处是复杂度更高的解法
                 //     dp[i][j] += dp[i - 1][j - k * coins[i]];
                 // }
-                dp[i][j] = dp[i - 1][j];
-                if (j - coins[i] >= 0) {
-                    dp[i][j] += dp[i][j - coins[i]];
+                if(j - coins[i] >= 0){
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i]];
+                }
+                else{
+                    dp[i][j] = dp[i - 1][j];
                 }
             }
         }
-        return dp[len - 1][amount];
+        return dp[coins.length - 1][amount];
     }
 }
