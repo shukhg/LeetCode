577458526fc8c3bdfde083e3276a3626ea909267
luweihai <644475880@qq.com>
2020-03-16 15:06:30 +0800

    2020-03-16 15

--- Solution/【007】整数反转/整数反转.java
+++ Solution/【007】整数反转/整数反转.java
@@ -1,7 +1,7 @@
 /*
-1、  Integer.MAX_VALUE      Integer.MIN_VALUE 
-2、  注意整数反转的算法，其实就一个while循环，要理解
-3、  用long的因为防止超过int的界限 
+（1）Integer.MAX_VALUE      Integer.MIN_VALUE 
+（2）注意整数反转的算法，其实就一个while循环，要理解
+（3）用long的因为防止超过int的界限 
 */
 
 
