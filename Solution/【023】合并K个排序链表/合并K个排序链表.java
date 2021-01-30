8b1b79aa3da004998bc988a4b3c94960507e5373
luweihai <644475880@qq.com>
2021-01-30 23:11:49 +0800

    K 个排序链表的合并

--- Solution/【023】合并K个排序链表/合并K个排序链表.java
+++ Solution/【023】合并K个排序链表/合并K个排序链表.java
@@ -45,6 +45,45 @@ class Solution {
 
 
 
+// 二分
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        return merge(lists, 0, lists.length - 1);
+    }
+
+    public ListNode merge(ListNode[] lists, int l, int r) {
+        if (l == r) {
+            return lists[l];
+        }
+        if (l > r) {
+            return null;
+        }
+        int mid = (l + r) >> 1;
+        return mergeTwoLists(merge(lists, l, mid), merge(lists, mid + 1, r));
+    }
+
+    public ListNode mergeTwoLists(ListNode a, ListNode b) {
+        if (a == null || b == null) {
+            return a != null ? a : b;
+        }
+        ListNode head = new ListNode(0);
+        ListNode tail = head, aPtr = a, bPtr = b;
+        while (aPtr != null && bPtr != null) {
+            if (aPtr.val < bPtr.val) {
+                tail.next = aPtr;
+                aPtr = aPtr.next;
+            } else {
+                tail.next = bPtr;
+                bPtr = bPtr.next;
+            }
+            tail = tail.next;
+        }
+        tail.next = (aPtr != null ? aPtr : bPtr);
+        return head.next;
+    }
+}
+
+
 
 // 最简单的办法，所有元素存入 list 然后排序，最后生成结点
 class Solution {
