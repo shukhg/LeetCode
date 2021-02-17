3968159bfbcc9d5a56dbfac21cdeab1446d45d9d
luweihai <644475880@qq.com>
2021-02-17 17:53:19 +0800

    111

--- Solution/【069】x的平方根/x的平方根.java
+++ Solution/【069】x的平方根/x的平方根.java
@@ -27,3 +27,16 @@ public class Solution {
         return (int) left;
     }
 }
+
+
+// 牛顿迭代法
+public class Solution {
+
+    public int mySqrt(int a) {
+        long x = a;
+        while (x * x > a) {
+            x = (x + a / x) / 2;
+        }
+        return (int) x;
+    }
+}
